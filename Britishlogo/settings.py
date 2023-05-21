6c18dbd637c457158e94f538bcc7008db69c27cd
Supermathew <dolikemathewalex@gmail.com>
2023-05-21 21:41:17 +0530

    csrf

--- Britishlogo/settings.py
+++ Britishlogo/settings.py
@@ -31,7 +31,11 @@ SECRET_KEY = env("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env("DJANGO_DEBUG")
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["localhost","127.0.0.1","https://britishlogoproject-production.up.railway.app"]
+
+
+CSRF_TRUSTED_ORIGINS = ["https://britishlogoproject-production.up.railway.app"]
+
 
 
 # Application definition
